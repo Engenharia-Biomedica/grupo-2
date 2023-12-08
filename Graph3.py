f611a211e6daccfd0eb29f77b265a3a101d8ed67
Vinicius Ferri <142631368+FerriVinicius@users.noreply.github.com>
2023-12-08 00:31:53 -0300

    Update Graph3.py

--- Graph3.py
+++ Graph3.py
@@ -1,24 +1,28 @@
+##bibliotecas úteis para o funcionamento do sistema
 import pandas as pd
 import streamlit as st
 import numpy as np
 import altair as alt
 
+
+##função que será chamada na página principal
 def graph3():
     st.header("Quantidade Total de Casos e Ranking de Alas por Infecções")
 
+    ##divisão por colunas para melhor visualização
     col1, col2, col3 = st.columns([1, 1, 2])
 
-    # Carregar os dados do CSV
+    ##carrega os dados do CSV
     url = "https://raw.githubusercontent.com/AndersonEduardo/pbl-2023/main/sample_data_clean.csv"
     df = pd.read_csv(url)
 
-    # Converter a coluna de datas
+    ##converte a coluna de datas
     df['dh_recebimento_exame'] = pd.to_datetime(df['dh_recebimento_exame'])
 
-    # Botões para seleção de período
+    ##botões para seleção de período
     periodo_opcao = col1.radio("3- Selecione o período:", ["30 dias", "90 dias", "6 meses", "1 ano", "Período completo"])
 
-    # Calcular o período correspondente
+    ##calcula o período para a seleção dos botões correspondentes
     if periodo_opcao == "30 dias":
         data_maxima = df['dh_recebimento_exame'].max().date()
         data_inicial = data_maxima - pd.DateOffset(days=30)
@@ -35,29 +39,29 @@ def graph3():
         data_inicial = df['dh_recebimento_exame'].min().date()
         data_maxima = df['dh_recebimento_exame'].max().date()
 
-    # Adicionar filtro de período
+    ##adiciona filtro de período personalizado
     filtro_periodo = col1.date_input('3- Selecione o período:', [data_inicial, data_maxima])
 
-    # Converter para numpy.datetime64
+    ##converte os filtros de período para numpy.datetime64, para posterior ajuste de valores de média em gráficos
     filtro_periodo = [np.datetime64(date) for date in filtro_periodo]
 
-    # Aplicar filtro de período
+    ##plica filtro de período selecionado
     df_filtrado = df[(df['dh_recebimento_exame'] >= filtro_periodo[0]) & (df['dh_recebimento_exame'] <= filtro_periodo[1])]
 
-    # Contabilizar a quantidade total de casos
+    ##contabiliza a quantidade total de casos
     total_casos = len(df_filtrado)
 
-    # Ranquear as alas por maior número de infecções
+    ##rankeia as alas por maior número de infecções
     ranking_alas = df_filtrado['ds_ala_coleta'].value_counts().reset_index()
     ranking_alas.columns = ['Ala', 'Número de Infecções']
     ranking_alas = ranking_alas.sort_values(by='Número de Infecções', ascending=False)
 
-    # Ranquear os microorganismos mais encontrados
+    ##rankeia os microorganismos mais encontrados
     ranking_microorganismos = df_filtrado['cd_sigla_microorganismo'].value_counts().reset_index()
     ranking_microorganismos.columns = ['Microorganismo', 'Número de Casos']
     ranking_microorganismos = ranking_microorganismos.sort_values(by='Número de Casos', ascending=False)
 
-    # Exibir resultados
+    ##exibição dos resultados obtidos pelos rankeamentos feitos
     col2.write(f"Quantidade Total de Casos: {total_casos}")
     col3.dataframe(ranking_alas, use_container_width=True, hide_index=True)
     col2.write("Ranking de Microorganismos mais Encontrados:")
